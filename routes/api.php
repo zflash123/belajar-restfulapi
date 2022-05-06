5d758782e53c1a691e6be96f8069ea9fdbe47b50
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-05-06 19:52:56 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/posts', function() {
+    return Post::all();
+});
+
+Route::post('/posts', function() {
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+
+    return Post::create([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+});
+
+Route::put('/posts/{post}', function(Post $post) {
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+    
+    $test = $post->update([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+
+    return [
+        'berhasil' => $test
+    ];
+});
+
+Route::delete('/posts/{post}', function(Post $post) {
+    $test = $post->delete();
+
+    return [
+        'berhasil' => $test
+    ];
+});
